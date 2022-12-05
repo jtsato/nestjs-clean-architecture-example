0ca51ba6aaad9540cf41c7a47d0d11f1a74b5185
jtsato <jorge.takeshi.sato@gmail.com>
2022-12-05 00:18:10 -0300

    feat(app): create optional class type

---
+++ src/core/commons/optional.spec.ts
@@ -0,0 +1,108 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import { Optional } from './optional';
+
+describe('Optional', () => {
+    describe('empty', () => {
+        it('should return an empty Optional', () => {
+            const optional = Optional.empty();
+            expect(optional.isPresent()).toBe(false);
+            expect(() => optional.get()).toThrow('No value present');
+        });
+    });
+
+    describe('of', () => {
+        it('should throw an error if the given value is null', () => {
+            const optional = Optional.of(null as string);
+            expect(optional.isPresent()).toBe(false);
+            expect(() => optional.get()).toThrow('No value present');
+        });
+
+        it('should return an Optional with the given value', () => {
+            const optional = Optional.of('foo');
+            expect(optional.isPresent()).toBe(true);
+            expect(optional.get()).toBe('foo');
+        });
+    });
+
+    describe('ofNullable', () => {
+        it('should return an Optional with the given value', () => {
+            const optional = Optional.ofNullable('foo');
+            expect(optional.isPresent()).toBe(true);
+            expect(optional.get()).toBe('foo');
+        });
+
+        it('should return an empty Optional', () => {
+            const optional = Optional.ofNullable(null as string);
+            expect(optional.isPresent()).toBe(false);
+            expect(() => optional.get()).toThrow('No value present');
+        });
+    });
+
+    describe('ifPresent', () => {
+        it('should call the given function if the value is present', () => {
+            const optional = Optional.of('foo');
+            const spy = jest.fn();
+            optional.ifPresent(spy);
+            expect(spy).toHaveBeenCalledWith('foo');
+        });
+
+        it('should not call the given function if the value is not present', () => {
+            const optional = Optional.empty();
+            const spy = jest.fn();
+            optional.ifPresent(spy);
+            expect(spy).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('orElse', () => {
+        it('should return the value if present', () => {
+            const optional = Optional.of('foo');
+            expect(optional.orElse('bar')).toBe('foo');
+        });
+
+        it('should return the given value if not present', () => {
+            const optional = Optional.empty();
+            expect(optional.orElse('bar')).toBe('bar');
+        });
+    });
+
+    describe('orElseGet', () => {
+        it('should return the value if present', () => {
+            const optional = Optional.of('foo');
+            expect(optional.orElseGet(() => 'bar')).toBe('foo');
+        });
+
+        it('should return the given value if not present', () => {
+            const optional = Optional.empty();
+            expect(optional.orElseGet(() => 'bar')).toBe('bar');
+        });
+    });
+
+    describe('orElseThrow', () => {
+        it('should return the value if present', () => {
+            const optional = Optional.of('foo');
+            expect(optional.orElseThrow(Error)).toBe('foo');
+        });
+
+        it('should throw the given error if not present', () => {
+            const optional = Optional.empty();
+            expect(() => optional.orElseThrow(Error)).toThrow(Error);
+        });
+    });
+
+    describe('map', () => {
+        it('should return an Optional with the result of the given function', () => {
+            const optional = Optional.of('foo');
+            const result = optional.map((value) => value.toUpperCase());
+            expect(result.isPresent()).toBe(true);
+            expect(result.get()).toBe('FOO');
+        });
+
+        it('should return an empty Optional if the value is not present', () => {
+            const optional = Optional.of(null as string);
+            const result = optional.map((value) => value.toUpperCase());
+            expect(result.isPresent()).toBe(false);
+            expect(() => result.get()).toThrow('No value present');
+        });
+    });
+});
---
+++ src/core/commons/optional.ts
@@ -0,0 +1,57 @@
+export class Optional<T> {
+    private readonly value: T;
+
+    private constructor(value: T) {
+        this.value = value;
+    }
+
+    public static empty<T>(): Optional<T> {
+        return new Optional<T>(null);
+    }
+
+    public static of<T>(value: T): Optional<T> {
+        return new Optional(value);
+    }
+
+    public get(): T {
+        if (!this.isPresent()) {
+            throw new Error('No value present');
+        }
+        return this.value;
+    }
+
+    public static ofNullable<T>(value: T): Optional<T> {
+        return value !== null ? Optional.of(value) : Optional.empty();
+    }
+
+    public isPresent(): boolean {
+        return this.value !== null;
+    }
+
+    public ifPresent(method: (value: T) => void): void {
+        if (this.isPresent()) method(this.value);
+    }
+
+    public orElse(other: T): T {
+        return this.isPresent() ? this.value : other;
+    }
+
+    public orElseGet(method: () => T): T {
+        return this.isPresent() ? this.value : method();
+    }
+
+    public orElseThrow<E extends Error>(error: new () => E): T {
+        if (this.isPresent()) {
+            return this.value;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-throw-literal, new-cap
+        throw new error();
+    }
+
+    public map<R>(mapper: (value: T) => R): Optional<R> {
+        if (!this.isPresent()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(mapper(this.value));
+    }
+}
