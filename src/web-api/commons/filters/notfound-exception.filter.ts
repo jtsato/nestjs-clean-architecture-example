1c002b5de99cdb711af5333feb617690ae343274
jtsato <jorge.takeshi.sato@gmail.com>
2022-12-14 02:22:01 -0300

    fix(duplication): refactoring duplicated code

--- src/web-api/commons/filters/notfound-exception.filter.ts
+++ src/web-api/commons/filters/notfound-exception.filter.ts
@@ -1,9 +1,9 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus, Logger } from '@nestjs/common';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 import { AbstractHttpAdapter, HttpAdapterHost } from '@nestjs/core';
-import { I18nContext } from 'nestjs-i18n';
 import { NotFoundException } from '@/core/exceptions';
 import { ResponseStatus } from '@/web-api/commons/models';
+import { MessageFormatterHelper } from './message-formatter.helper';
 
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter<NotFoundException> {
@@ -19,19 +19,9 @@ export class NotFoundExceptionFilter implements ExceptionFilter<NotFoundExceptio
         const response = context.getResponse<Response>();
         const status: number = HttpStatus.NOT_FOUND;
         const key = `messages.${exception.message}`;
-        const message: string = this.getErrorMessage(key, exception.parameters);
+        const message: string = MessageFormatterHelper.format(key, exception.parameters);
         const body: ResponseStatus = new ResponseStatus(status, message, []);
         this.logger.warn(JSON.stringify(body));
         this.httpAdapter.reply(response, body, status);
-    }
-
-    private getErrorMessage(key: string, parameters: Array<any>): string {
-        let message: string = I18nContext.current().translate(key);
-        for (let index = 0; index < parameters.length; index += 1) {
-            const parameter: string = parameters[index] as unknown as string;
-            message = message.replace(`{${index}}`, parameter);
-        }
-
-        return message;
     }
 }
--- src/web-api/commons/filters/unique-constraint.filter.ts
+++ src/web-api/commons/filters/unique-constraint.filter.ts
@@ -1,16 +1,16 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus, Logger } from '@nestjs/common';
 import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 import { AbstractHttpAdapter, HttpAdapterHost } from '@nestjs/core';
-import { I18nContext, I18nService } from 'nestjs-i18n';
 import { UniqueConstraintException } from '@/core/exceptions';
 import { ResponseStatus } from '@/web-api/commons/models';
+import { MessageFormatterHelper } from './message-formatter.helper';
 
 @Catch(UniqueConstraintException)
 export class UniqueConstraintExceptionFilter implements ExceptionFilter<UniqueConstraintException> {
     private readonly logger = new Logger(UniqueConstraintException.name);
     private readonly httpAdapter: AbstractHttpAdapter;
 
-    constructor(adapterHost: HttpAdapterHost, i18n: I18nService) {
+    constructor(adapterHost: HttpAdapterHost) {
         this.httpAdapter = adapterHost.httpAdapter;
     }
 
@@ -18,21 +18,10 @@ export class UniqueConstraintExceptionFilter implements ExceptionFilter<UniqueCo
         const context: HttpArgumentsHost = host.switchToHttp();
         const response = context.getResponse<Response>();
         const key = `messages.${exception.message}`;
-        const message: string = this.getErrorMessage(key, exception.parameters);
+        const message: string = MessageFormatterHelper.format(key, exception.parameters);
         const status: number = HttpStatus.BAD_REQUEST;
         const body: ResponseStatus = new ResponseStatus(status, message, []);
         this.logger.warn(JSON.stringify(body));
         this.httpAdapter.reply(response, body, status);
-    }
-
-    private getErrorMessage(key: string, parameters: Array<any>): string {
-        let message: string = I18nContext.current().translate(key);
-
-        for (let index = 0; index < parameters.length; index += 1) {
-            const parameter: string = parameters[index] as unknown as string;
-            message = message.replace(`{${index}}`, parameter);
-        }
-
-        return message;
     }
 }
