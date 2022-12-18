d705041aa82a2a4adedfb215fb5a7a34e8c1d6e9
jtsato <jorge.takeshi.sato@gmail.com>
2022-12-17 21:52:18 -0300

    fix(mutation): fix mutation testing issues

--- src/core/commons/helpers/date-time.helper.ts
+++ src/core/commons/helpers/date-time.helper.ts
@@ -1,8 +1,7 @@
 export class DateTimeHelper {
-    private static timeZoneOffsetInHours = 180 * 60 * 1000;
-
     static toLocalISOString(date: Date): string {
-        const localDate = new Date(date.getTime() - DateTimeHelper.timeZoneOffsetInHours);
+        const offsetInHours = date.getTimezoneOffset() * 60 * 1000;
+        const localDate = new Date(date.getTime() - offsetInHours);
         const isoString = localDate.toISOString();
         return isoString.split('.')[0].replace('T', ' ');
     }
