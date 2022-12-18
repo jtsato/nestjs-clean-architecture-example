4987b2b00907740ff3404058857092095e005bba
jtsato <jorge.takeshi.sato@gmail.com>
2022-12-17 22:46:56 -0300

    fix(mutation): fix mutation testing issues

--- src/core/commons/helpers/date-time.helper.ts
+++ src/core/commons/helpers/date-time.helper.ts
@@ -1,7 +1,6 @@
 export class DateTimeHelper {
     static toLocalISOString(date: Date): string {
         const offsetInHours = date.getTimezoneOffset() * 60 * 1000;
-        console.log('offsetInHours', offsetInHours);
         const localDate = new Date(date.getTime() - offsetInHours);
         const isoString = localDate.toISOString();
         return isoString.split('.')[0].replace('T', ' ');
